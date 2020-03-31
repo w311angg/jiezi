372df64aced592d934160c5302dd9600e0e50297
RZ <30381083+w311ang@users.noreply.github.com>
2020-03-31 17:09:49 +0800

    Create 芥子签到.sh

---
+++ 芥子签到.sh
@@ -0,0 +1,46 @@
+#资源分享
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=92"
+#薅羊毛
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=104"
+#游戏破解
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=101"
+#游戏圈
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=4"
+#Sky光·遇
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=116"
+#话题圈
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=102"
+#技术学院
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=115"
+#营养快线
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=106"
+#死肥宅
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=117"
+#仙女下凡
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=3"
+#玩机一族
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=100"
+#王者荣耀
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=110"
+#刺激战场
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=111"
+#地下城与勇士
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=112"
+#影视音乐
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=97"
+#游戏开发
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=114"
+#脚本开发
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=45"
+#许愿
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=64"
+#水吧
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=2"
+#宣传部
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=99"
+#公告
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=1"
+#意见反馈
+curl "https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key=[_key]&cat_id=6"
+#云挂机签到
+curl "https://api.lieyou888.com/signin/create/ANDROID/1.0?_key=[_key]"
