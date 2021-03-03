c635ed4be161766ccea318ea7ad296d7057d2ceb
RZ <30381083+w311ang@users.noreply.github.com>
2021-03-03 21:31:40 +0800

    Update jiezi.py

--- jiezi.py
+++ jiezi.py
@@ -13,13 +13,23 @@ for i in 板块列表["categories"]:
       成功=成功+1
     else:
       失败=失败+1
-云挂机回返=requests.post("https://api.lieyou888.com/signin/create/ANDROID/1.0?_key="+密钥).text
-print("云挂机："+云挂机回返)
-if json.loads(云挂机回返)["status"]==1:
-  成功=成功+1
-else:
-  失败=失败+1
+状态=[]
+云挂机回返=requests.post("https://api.lieyou888.com/signin/create/ANDROID/1.0?_key="+密钥)
+print("云挂机："+云挂机回返.text)
+状态.append(云挂机回返.json()['status'])
+
 一元签到=requests.get('https://api.market.lieyou888.com/sign/click/ANDROID/1.0?_key='+密钥).json()
 print(一元签到)
-requests.post('https://api.bbs.lieyou888.com/post/create/ANDROID/1.0?_key='+密钥,data={'lng':0.0,'cat_id':2,'tag_id':'-1','detail':'水鸭子好看吗','type':0,'title':'你这个漂亮的小水鸭','lat':0.0})
+状态.append(一元签到['status'])
+
+一元发帖=requests.post('https://api.bbs.lieyou888.com/post/create/ANDROID/1.0?_key='+密钥,data={'lng':0.0,'cat_id':2,'tag_id':'-1','detail':'水鸭子好看吗','type':0,'title':'你这个漂亮的小水鸭','lat':0.0}).json()
+print(一元发帖)
+#状态.append(一元发帖['status'])
+
+一元任务=requests.get('https://api.market.lieyou888.com/task/daily/list/ANDROID/1.0?_key='+密钥).json()['list']
+for one in 一元任务:
+  id=one['id']
+  返回=requests.get(’https://api.market.lieyou888.com/task/receive/ANDROID/1.0?_key='+密钥+'&task_id='+id).json()
+  print(one['title']+'：'+返回)
+  状态.append(返回['status'])
 print(str(成功)+" succeeded, "+str(失败)+" failed.")
