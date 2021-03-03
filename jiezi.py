b877a0f291ffba24b0d4e52d679aa747270c30be
RZ <30381083+w311ang@users.noreply.github.com>
2021-03-03 23:29:59 +0800

    Update jiezi.py

--- jiezi.py
+++ jiezi.py
@@ -9,7 +9,7 @@ print('\n===开始板块签到===')
 for i in 板块列表["categories"]:
   if i["categoryID"]!=0:
     板块签到回返=requests.get("https://api.bbs.lieyou888.com/user/signin/ANDROID/1.0?_key="+密钥+"&cat_id="+str(i["categoryID"])).text
-    print(i["title"]+"："+板块签到回返)
+    print(i["title"]+"："+json.loads(板块签到回返)["msg"])
     if json.loads(板块签到回返)["status"]==1:
       成功=成功+1
     else:
@@ -20,31 +20,31 @@ print('\n===开始其他任务===')
 成功=0
 失败=0
 状态=[]
-云挂机回返=requests.post("https://api.lieyou888.com/signin/create/ANDROID/1.0?_key="+密钥)
-print("云挂机签到："+云挂机回返.text)
-状态.append(云挂机回返.json()['status'])
+云挂机回返=requests.post("https://api.lieyou888.com/signin/create/ANDROID/1.0?_key="+密钥).json()
+print("云挂机签到："+云挂机回返['msg'])
+状态.append(云挂机回返['status'])
 
 一元签到=requests.get('https://api.market.lieyou888.com/sign/click/ANDROID/1.0?_key='+密钥).json()
-print('一元签到：'+str(一元签到))
+print('一元签到：'+一元签到['msg'])
 状态.append(一元签到['status'])
 
 一言=requests.get('https://v1.hitokoto.cn/').json()['hitokoto']
 发帖=requests.post('https://api.bbs.lieyou888.com/post/create/ANDROID/1.0?_key='+密钥,data={'lng':0.0,'cat_id':2,'tag_id':'-1','detail':一言,'type':0,'title':一言,'lat':0.0}).json()
-print('发帖：'+str(发帖))
+print('发帖：'+发帖['msg'])
 状态.append(发帖['status'])
 
 分享=requests.get('https://api.market.lieyou888.com/task/perform/ANDROID/1.0?data_type=APP_SHARE&_key='+密钥).json()
-print('分享：'+str(分享))
+print('分享：'+分享['msg'])
 状态.append(分享['status'])
 
 帖子=requests.get('https://api.bbs.lieyou888.com/post/list/ANDROID/1.1?cat_id=2').json()['posts']
 for one in 帖子:
   点赞=requests.get('https://api.bbs.lieyou888.com/post/praise/ANDROID/1.1?post_id='+str(one['postID'])+'&_key='+密钥).json()
-  print('点赞：'+str(点赞))
+  print('点赞：'+点赞['msg'])
   状态.append(点赞['status'])
 
 回复=requests.post('https://api.bbs.lieyou888.com/comment/create/ANDROID/1.0?_key='+密钥,data={'text':一言,'post_id':帖子[3]['postID'],'comment_id':0}).json()
-print('回复：'+str(回复))
+print('回复：'+回复['msg'])
 状态.append(回复['status'])
 
 for one in 状态:
@@ -62,7 +62,7 @@ print('\n===开始领取一元任务奖励===')
 for one in 一元任务:
   id=one['id']
   返回=requests.get('https://api.market.lieyou888.com/task/receive/ANDROID/1.0?_key='+密钥+'&task_id='+str(id)).json()
-  print(one['title']+'：'+str(返回))
+  print(one['title']+'：'+返回['msg'])
   状态.append(返回['status'])
 未完成=[]
 for one in 一元任务:
