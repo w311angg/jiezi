b6f569acb99f9874402f0c609faa281f5e637f7c
RZ <30381083+w311ang@users.noreply.github.com>
2021-03-04 21:58:59 +0800

    Update jiezi.py

--- jiezi.py
+++ jiezi.py
@@ -59,6 +59,7 @@ print('删回复：'+删回复['msg'])
 
 登录游戏=requests.post('https://sdkapi.1yuan.cn/sdkapi/user/login/gameuser',data=os.getenv('login')).json()
 print('登录游戏：'+登录游戏['message'])
+print(登录游戏)
 状态.append(登录游戏['data']['popupStatus'])
 
 for one in 状态:
