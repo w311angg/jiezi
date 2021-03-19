0066bcfd3b5ad8b2217006546d4d84efb55fb58c
RZ <30381083+w311ang@users.noreply.github.com>
2021-03-19 16:30:21 +0800

    Update jiezi.py

--- jiezi.py
+++ jiezi.py
@@ -124,11 +124,11 @@ for one in 一元任务:
 for one in 一元成就:
   id=one['id']
   返回=requests.get('https://api.market.lieyou888.com/task/receive/ANDROID/1.0?_key='+密钥+'&task_id='+str(id)).json()
-  print(one['title']+'：'+返回['msg'])
+  print('%s (%s/%s)：%s'%(one['title'],one['completedQuantity'],one['targetQuantity'],返回['msg'])
   状态.append(返回['status'])
 for one in 一元成就:
   if one['finished']==0:
-    未完成.append(one['title'])
+    未完成.append('%s (%s/%s)'%(one['title'],one['completedQuantity'],one['targetQuantity']))
 for one in 状态:
   if one==1:
     成功+=1
