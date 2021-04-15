2cf020fa9133e37d698adcee2ebdd62c27e027b4
RZ <30381083+w311ang@users.noreply.github.com>
2021-04-15 21:22:09 +0800

    Create hlx.py

---
+++ hlx.py
@@ -0,0 +1,10 @@
+import requests
+
+plist=requests.get('http://floor.huluxia.com/post/list/ANDROID/2.1?start=0&count=1&cat_id=43&tag_id=0&sort_by=0').json()['posts']
+posts=[]
+
+for one in plist:
+  pid=one['postID']
+  title=one['title']
+  content=requests.get('http://floor.huluxia.com/post/detail/ANDROID/2.3?post_id='+pid)
+  posts.append({'title':title,'content':content})
